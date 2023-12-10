e71b240f3049033af28a36e4c2bab92aa2e63cf7
Andres Seivane <andyseivane@hotmail.com>
2023-12-09 21:55:48 -0300

    pihole con NGINX

---
+++ pihole/README.md
@@ -0,0 +1,5 @@
+No instalar el servidor web
+
+Cambiar la constraseña generada automaticamente.
+
+Basado en https://docs.pi-hole.net/guides/webserver/nginx/
--- pihole/install.sh
+++ pihole/install.sh
@@ -1,4 +1,22 @@
-#! /bin/bash
+#! /bin/env
 git clone --depth 1 https://github.com/pi-hole/pi-hole.git ~/Pi-hole
 cd ~/Pi-hole/automated\ install/
 sudo bash basic-install.sh
+wait
+
+sudo apt-get -y install php7.4-fpm php7.4-cgi php7.4-xml php7.4-sqlite3 php7.4-intl apache2-utils
+
+systemctl enable php7.4-fpm
+
+# Change ownership of the html directory to nginx user
+
+sudo chown -R www-data:www-data /var/www/html
+
+# Make sure the html directory is writable
+sudo chmod -R 755 /var/www/html
+
+# Grant the admin panel access to the gravity database
+sudo usermod -aG pihole www-data
+
+#Start php7.3-fpm daemon
+sudo systemctl start php7.4-fpm
